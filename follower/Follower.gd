cc57ec0984f58b30bb19e40b7f60e00acfeb35fd
Schoperation <cschop1@gmail.com>
2020-12-07 14:52:47 -0500

    Worked on pathfinding bit more

--- follower/Follower.gd
+++ follower/Follower.gd
@@ -16,4 +16,13 @@ func _process(delta):
 # Travel to the player... so far just straight lines	
 func pathfind():
 	
+	var distanceZ = player.transform.origin.z - self.transform.origin.z
+	var distanceX = player.transform.origin.x - self.transform.origin.x
 	var distance = self.transform.origin.distance_to(player.transform.origin)
+	var travelZ = distanceZ / 50
+	var travelX = distanceX / 50
+	
+	if abs(distanceX) > 3:
+		translate(Vector3(travelX, 0, 0))
+	if abs(distanceZ) > 3:
+		translate(Vector3(0, 0, travelZ))
