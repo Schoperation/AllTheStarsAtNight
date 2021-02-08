17a7759cc57504c2fa7bf59ea1df44eed61e2dc7
Schoperation <cschop1@gmail.com>
2021-02-08 15:03:11 -0500

    Fixed follower bug for sure

--- follower/Follower.gd
+++ follower/Follower.gd
@@ -18,45 +18,26 @@ func _process(delta):
 # Travel to the player... so far just straight lines	
 func pathfind():
 	
-	# Calc distance to player
-	var distanceZ = player.transform.origin.z - self.transform.origin.z
-	var distanceX = player.transform.origin.x - self.transform.origin.x
-	var distance = self.transform.origin.distance_to(player.transform.origin)
-	var travelZ = distanceZ / 50
-	var travelX = distanceX / 50
-	
-	if abs(distanceX) > 3:
-		translate(Vector3(travelX, 0, 0))
-	if abs(distanceZ) > 3:
-		translate(Vector3(0, 0, travelZ))
-		
-	# Make sure they aren't stuck in each other
-	# Calc distance to other follower
+	# Let Barfite travel to the player and Vigil follow Barfite
 	if isBarfite:
+		var distanceZ = player.transform.origin.z - self.transform.origin.z
+		var distanceX = player.transform.origin.x - self.transform.origin.x
+		var travelZ = distanceZ / 50
+		var travelX = distanceX / 50
+		
+		if abs(distanceX) > 3:
+			translate(Vector3(travelX, 0, 0))
+		if abs(distanceZ) > 3:
+			translate(Vector3(0, 0, travelZ))
+	else:
 		var distance2Z = otherFollower.transform.origin.z - self.transform.origin.z
 		var distance2X = otherFollower.transform.origin.x - self.transform.origin.x
 		#var distance2 = self.transform.origin.distance_to(otherFollower.transform.origin)
-		var travel2Z = distance2Z / 50
-		var travel2X = distance2X / 50
+		var travel2Z = distance2Z / 30
+		var travel2X = distance2X / 30
 		
 		if abs(distance2X) > 3:
 			translate(Vector3(travel2X, 0, 0))
 		if abs(distance2Z) > 3:
 			translate(Vector3(0, 0, travel2Z)) 
-	"""
-	var distance2Z = otherFollower.transform.origin.z - self.transform.origin.z
-	var distance2X = otherFollower.transform.origin.x - self.transform.origin.x
-	var distance2 = self.transform.origin.distance_to(otherFollower.transform.origin)
-	var travel2Z = distanceZ / 50
-	var travel2X = distanceX / 50
-	
-	if abs(distance2X) > 3:
-		translate(Vector3(travel2X, 0, 0))
-	#elif abs(distance2X) < 2:
-	#	translate(Vector3(-1 * travel2X, 0, 0))
-	if abs(distance2Z) > 3:
-		translate(Vector3(0, 0, travel2Z))
-	#elif abs(distance2Z) < 2:
-	#	translate(Vector3(-1 * travel2Z, 0, 0))
-	"""
 	
