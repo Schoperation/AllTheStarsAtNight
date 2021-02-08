cc07f2915de932c75e9fcc3bc272770df4562cee
Schoperation <cschop1@gmail.com>
2021-02-08 18:56:28 -0500

    Changed name, added fps

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# WalkInTheWoods
+# All the Stars at Night
+
+Explore an in-depth forest with an in-depth duo
---
+++ ui/Version.gd
@@ -0,0 +1,13 @@
+extends RichTextLabel
+
+# Version
+var gameName = "All the Stars at Night"
+var desc = "Explore an in-depth forest with an in-depth duo"
+var version = "0.2.1"
+
+func _ready():
+	# Change version
+	self.text = gameName + " v" + version
+	
+func _process(delta):
+	$FPS.text = "FPS: " + str(Engine.get_frames_per_second())
